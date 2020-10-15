32a15d48336f1e1e0ef4aeffb1cee4ff5d304f53
Sarukii <allan.paccot@gmail.com>
2020-10-15 17:22:10 +0200

    gitignore change

--- SPI/Core/Src/main.c
+++ SPI/Core/Src/main.c
@@ -23,7 +23,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "SX1272.h"
-#define RECEIVER 0
+#define RECEIVER 1
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
