6eabf0f9e08b2764f8df4011244feccf89070004
ThibaultMollier <thibault.mollier@lilo.org>
2020-10-15 16:31:36 +0200

    define

--- SPI/SX1272/SX1272.c
+++ SPI/SX1272/SX1272.c
@@ -12,15 +12,15 @@ extern SPI_HandleTypeDef hspi1;
 //! @last_edit : 15/10/2020
 //! @details :
 void SX1272_Init(void) {
-	SX1272_WriteRegister(0x01, 0x00);
+	SX1272_WriteRegister(RegOpMode, FSK_SLEEP);
 
 	HAL_Delay(15);
-	SX1272_WriteRegister(0x01, 0x80);
+	SX1272_WriteRegister(RegOpMode, LORA_SLEEP);
 
 	HAL_Delay(15);
-	SX1272_WriteRegister(0x06, 0xD9); //Frequency 868.5MHz : 0xD92000
-	SX1272_WriteRegister(0x07, 0x20);
-	SX1272_WriteRegister(0x08, 0x00);
+	SX1272_WriteRegister(RegFrMsb, 0xD9); //Frequency 868.5MHz : 0xD92000
+	SX1272_WriteRegister(RegFrMib, 0x20);
+	SX1272_WriteRegister(RegFrLsb, 0x00);
 }
 
 //! @last_edit : 15/10/2020
@@ -37,6 +37,14 @@ void SX1272_WriteRegister(uint8_t reg, uint8_t val) {
 	while (HAL_SPI_GetState(&hspi1) != HAL_SPI_STATE_READY);
 
 	HAL_GPIO_WritePin(NSS_GPIO_Port, NSS_Pin, 1);
+}
+
+void SPI_WriteBurst(uint8_t reg, uint8_t* val, uint8_t len){
+    HAL_GPIO_WritePin(NSS_GPIO_Port, NSS_Pin, 0);
+    reg |= 0x80;
+    HAL_SPI_Transmit(&hspi1, &reg, 1, 1000);
+    HAL_SPI_Transmit(&hspi1, val, len, 1000);
+    HAL_GPIO_WritePin(NSS_GPIO_Port, NSS_Pin, 1);
 }
 
 //! @last_edit : 15/10/2020
@@ -70,59 +78,57 @@ void SX1272_BurstRead(uint8_t addr, uint8_t* rxBuf, uint8_t length) {
 
 //! @last_edit : 15/10/2020
 //! @details :
-void SX1272_Transmit(uint8_t tx) {
-	uint8_t addr = SX1272_ReadRegister(0x0E); // Tx base addr
+void SX1272_Transmit(uint8_t* Tx,uint8_t len){
+    uint8_t addr = SPI_Read_Register(RegFifoTxBaseAddr);//Tx base addr
+    SPI_Write_Register(RegFifoAddrPtr, addr);//write fifo addr ptr
+    SPI_Write_Register(RegPayloadLength, len);//Payload length
 
-	SX1272_WriteRegister(0x0D, addr); // write fifo addr ptr
+    HAL_Delay(15);
+    SPI_Write_Register(RegOpMode, LORA_STBY);
+    HAL_Delay(15);
 
-	HAL_Delay(15);
-	SX1272_WriteRegister(0x01, 0x81);
+    SPI_WriteBurst(ReqFifo,Tx,len);
 
-	HAL_Delay(15);
-	SX1272_WriteRegister(0x00, tx);
+    HAL_Delay(15);
+    SPI_Write_Register(RegOpMode, LORA_FSTX);
+    HAL_Delay(15);
+    SPI_Write_Register(RegOpMode, LORA_TX);
+    HAL_Delay(15);
 
-	HAL_Delay(15);
-	SX1272_WriteRegister(0x01, 0x82);
-
-	HAL_Delay(15);
-	SX1272_WriteRegister(0x01, 0x83);
-
-	HAL_Delay(15);
-	while((SX1272_ReadRegister(0x12) && 0x08)==0); // interrupt
-
-	SX1272_WriteRegister(0x12, 0xFF); // clear interrupt
+    while((SPI_Read_Register(RegIrqFlags) && 0x08)==0);//interrupt
+    SPI_Write_Register(RegIrqFlags, 0xFF);//clear interrupt
 }
 
 //! @last_edit : 15/10/2020
 //! @details :
 uint8_t SX1272_Receive(uint8_t rx[50]) {
 	uint8_t length = 0;
-	uint8_t addr = SX1272_ReadRegister(0x0F);//Rx base addr
-	SX1272_WriteRegister(0x0D, addr);//write fifo addr ptr
+	uint8_t addr = SX1272_ReadRegister(RegFifoRxBaseAddr);//Rx base addr
+	SX1272_WriteRegister(RegFifoAddrPtr, addr);//write fifo addr ptr
 
 	HAL_Delay(15);
-	SX1272_WriteRegister(0x01, 0x81);
+	SX1272_WriteRegister(RegOpMode, LORA_STBY);
 	HAL_Delay(15);
-	SX1272_WriteRegister(0x01, 0x84);
+	SX1272_WriteRegister(RegOpMode, LORA_FSRX);
 	HAL_Delay(15);
-	SX1272_WriteRegister(0x01, 0x85);
+	SX1272_WriteRegister(RegOpMode, LORA_RX_CONTINUOUS);
 	HAL_Delay(15);
 
 
-	while((SX1272_ReadRegister(0x12) && 0xC0)==0);//interrupt
+	while((SX1272_ReadRegister(RegIrqFlags) && 0xC0)==0);//interrupt
 
-	uint8_t interrupt = SX1272_ReadRegister(0x12);
+	uint8_t interrupt = SX1272_ReadRegister(RegIrqFlags);
 	if(interrupt == 0x80){
 		return 0;
 	}
 
-	addr = SX1272_ReadRegister(0x10);//Rx current addr
-	length = SX1272_ReadRegister(0x13);//Rx current addr
+	addr = SX1272_ReadRegister(RegFifoRxCurrentAddr);//Rx current addr
+	length = SX1272_ReadRegister(RegRxNbBytes);//Rx current addr
 
-	SX1272_WriteRegister(0x0D, addr);//write fifo addr ptr
+	SX1272_WriteRegister(RegFifoAddrPtr, addr);//write fifo addr ptr
 
-	SX1272_BurstRead(0, rx, length);
-	SX1272_WriteRegister(0x12, 0xFF);//clear interrupt
+	SX1272_BurstRead(ReqFifo, rx, length);
+	SX1272_WriteRegister(RegIrqFlags, 0xFF);//clear interrupt
 	return length;
 
 }
